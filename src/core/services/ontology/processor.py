87a0e7f428c3c48a83fa1045367771bbb597d716
tayadj <tayadj@users.noreply.github.com>
2025-05-27 11:51:47 +0300

    Ontology processor enhancement

--- src/core/services/ontology/processor.py
+++ src/core/services/ontology/processor.py
@@ -1,4 +1,5 @@
 import llama_index
+import networkx
 
 
 
@@ -6,26 +7,54 @@ class OntologyProcessor:
 
 	def __init__(self):
 
+		self.graph = networkx.MultiDiGraph()
 		self.index = None
 		self.query_engine = None
 
-	def build(self, ontology):
+	def create_entity(self, entity: dict):
+	
+		head = entity.get("head")
+		relation = entity.get("relation")
+		tail = entity.get("tail")
+
+		if not self.graph.has_node(head):
+
+			self.graph.add_node(head)
+
+		if not self.graph.has_node(tail):
+
+			self.graph.add_node(tail)
+
+		self.graph.add_edge(head, tail, relation = relation)
+
+	def delete_entity(self, entity: dict):
+
+		head = entity.get("head")
+		relation = entity.get("relation")
+		tail = entity.get("tail")
+
+		if self.graph.has_edge(head, tail):
+			
+			for key, attributes in self.graph.get_edge_data(head, tail).items():
+
+				if attributes.get("relation") == relation:
+
+					self.graph.remove_edge(head, tail, key = key)	
+
+	def build(self):
 
 		self.index = llama_index.core.indices.knowledge_graph.KnowledgeGraphIndex([], llm = llama_index.core.Settings.llm)
 
-		for entity in ontology:
+		for head, tail, data in self.graph.edges(data = True):
 
-			triplet = (entity["head"], entity["relation"], entity["tail"])
+			triplet = (head, data.get("relation"), tail)
 			self.index.upsert_triplet(triplet, include_embeddings = False)
 
 	async def process(self, query):
 
 		if not self.index:
 
-			raise ValueError("Knowledge Graph Index hasn't built yet.")
-
-		if not self.query_engine:
-
+			self.build()
 			self.query_engine = self.index.as_query_engine(include_text = True, response_mode = "tree_summarize")
 
 		results = await self.query_engine.aquery(query)
