1562f67b3c330070eaf4f151eeb1786fa7924240
tayadj <tayadj@users.noreply.github.com>
2025-07-05 20:53:36 +0300

    Implemented simple tokenizer

--- src/core/nexus/services/vectorizer/tokenizer.py
+++ src/core/nexus/services/vectorizer/tokenizer.py
@@ -3,4 +3,24 @@
 
 class Tokenizer:
 
-	pass
+	def __init__(self):
+
+		self.token_to_index = {}
+		self.index_to_token = {}
+
+	@property
+	def size(self):
+
+		return len(self.token_to_index)
+
+	def fit(self, corpus):
+
+		vocabulary = {token for text in corpus for token in text.split()}
+
+		self.token_to_index = { word : index for index, word in enumerate(vocabulary, start = 1) }
+		self.token_to_index["<PADDING>"] = 0
+
+		self.index_to_token = { index : word for index, word in enumerate(vocabulary, start = 1) }
+		self.index_to_token[0] = ["<PADDING>"]
+
+		# Implement service token general support
